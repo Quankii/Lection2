d8f53918d993bb0e8e26d96f5fed835939482cda
Artem <rtmdlgj@gmail.com>
2025-08-27 21:58:09 +0300

    Завдання про найбiльше число

---
+++ Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+int[] nums = {1, 2, 3 ,4 , 6, 7};
+int find = 5;
+byte count = 0;
+int n = nums.Length;
+
+while (count < n)
+{
+    if (nums[count] == find)
+    {
+        Console.WriteLine(find + " is in nums");
+        break;
+    }
+
+    count++;
+
+}
